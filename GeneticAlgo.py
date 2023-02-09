8b4cbb455ccc48b54ec1b65d9d49a82c1d1417a8
Ayush Mishra <ayush.mishra.0724@gmail.com>
2023-02-09 23:55:18 +0530

    more fitness changes
    Includes a text file to contain best chromosome in each run

--- GeneticAlgo.py
+++ GeneticAlgo.py
@@ -15,10 +15,11 @@ total_batch_list = set()
 day_timeslot_dict = {'mon': [1, 2, 3, 4, 5, 6], 'tue': [7, 8, 9, 10, 11, 12],
                      'wed': [13,14, 15, 16, 17, 18], 'thu': [19, 20, 21, 22, 23, 24],
                      'fri': [ 25, 28, 29, 30, 31, 32]}
-lab_alloted = {'2':6,'4':7,'6':7,'8':6}
+lab_alloted = {2:6,4:7,6:7,8:6}
 subject_lab_credithour_dict = {}
 subject_credithour_dict = {}
 subject_batch_dict = {}
+subject_batch_ind_dict = {}
 subject_teacher_dict = {}
 course_type_dict = {}
 #--------------------------------------------#
@@ -49,7 +50,10 @@ def initializeTables():
                 subject_batch_dict[i] = [j]
             else:
                 subject_batch_dict[i].append(j)
-    print(subject_batch_dict)
+    
+    for i, j in zip(cp['Course_Code'], cp['Semester']):
+        subject_batch_ind_dict[i] = j
+
     for i, j in zip(cp['Course_Code'], cp['Faculty_id']):
         subject_teacher_dict[i] = j
 
@@ -110,42 +114,58 @@ for i in range(popz):
 # Fitness evaluation 
 
 # No Faculty should have been assigned two different classes at same time ( in same slot of day )
+# No Lab should be assigned to two different batches at same time
+
 # We count the number of conflicts / Violations made in the chromosome and add 1/1+c score to the chromosome's final eval score
 # if c is 0 the max value of 1 is added 
 
 def fitnessFunction(chromosome):
-    conflicts = 0
-    def facultyclash(week):
+    total_val = 0
+    hconflicts = 0
+    sconflicts = 0
+    def hardConstraints(week):
         c = 0
         for day in week:
             for slot in day:
                 for sub in slot:
                     for osub in slot:
-                        if sub!='' and osub!='':    
+                        if sub!='' and osub!='':   
+                            # Faculty clash check
                             if sub!=osub and subject_teacher_dict[sub]==subject_teacher_dict[osub]:
                                 c+=1
+                            # Lab clash check
+                            if sub!=osub and course_type_dict[sub]=='L' and course_type_dict[osub]=='L':   
+                                if lab_alloted[subject_batch_ind_dict[sub]] == lab_alloted[subject_batch_ind_dict[osub]]:
+                                    c+=1         
         return c//2
-    
-    def labsClash(week):
-        c = 0 
+
+    def softContraints(week):
+        c = 0
         for day in week:
             for slot in day:
                 for sub in slot:
                     for osub in slot:
-                        if sub!='' and osub!='':    
-                            if sub!=osub and course_type_dict[sub]==course_type_dict[osub]:
-                                if 
+                        if sub!='' and osub!='':   
+                            
+                            
         return c//2
+    
 
-    conflicts = facultyclash(chromosome)
+    hconflicts += hardConstraints(chromosome)
     # Add more conflicts here 
+    total_val += 1/(1+hconflicts)
+    total_val += 1/(2+sconflicts)
 
-    return (1/(1+conflicts))
+    return (1/(1+hconflicts))
 
 # Fitness Calculations
 Fit_values = []
 for chromosome in pop:
     Fit_values.append(fitnessFunction(chromosome))
 
-print(*Fit_values)
-        
+with open("best_chromosome.txt", "a") as f:
+   f.write(str(pop[Fit_values.index(max(Fit_values))]))
+   f.write("\n")
+
+for i in range(len(pop)):
+    print(pop[i],Fit_values[i])
