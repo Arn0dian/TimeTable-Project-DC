580c2e2b0c00c8d9943233cf701c0e0c20d37f2a
Ayush Mishra <46701430+Arn0dian@users.noreply.github.com>
2023-02-17 03:21:45 +0530

    Add files via upload

---
+++ Modularization/geneticAlgo.py
@@ -0,0 +1,80 @@
+import time
+from initialization import *
+from fitness import *
+from crossover import *
+
+st = time.time()
+
+generations = 100
+gnc = 1
+while generations!=0:
+    generations -= 1
+    # Will make new child population 
+    childrenpop = []
+    childFit_value = []
+
+    temppop = pop.copy()
+    while temppop!=[]:
+        childrens = uniformCrossover(temppop)
+        if childrens!=[]:
+            o1,o2 = childrens[0],childrens[1]
+            childrenpop.append(o1)
+            childFit_value.append(fitnessFunction(o1))
+            childrenpop.append(o2)
+            childFit_value.append(fitnessFunction(o2))
+
+
+    pop += childrenpop
+    Fit_values += childFit_value
+    spop = [val for (_, val) in sorted(zip(Fit_values, pop), key=lambda x: x[0],reverse=True)]
+    pop = spop.copy()
+    pop = pop[:popz]
+    Fit_values.clear()
+    for i in pop:
+        Fit_values.append(fitnessFunction(i))
+    
+
+    print("Generation ",gnc)
+    gnc+=1
+    # import os, psutil; print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+
+
+def separateChromosome(chromosome):
+    sem2 = {}
+    sem4 = {}
+    sem6 = {}
+    sem8 = {}
+    dayMap = {1:"Mon", 2:"Tue" , 3:"Wed" , 4:"Thurs" , 5:"Fri"}
+    for i in range(len(chromosome)):
+        
+        sem2[dayMap[i+1]] = []
+        sem4[dayMap[i+1]] = []
+        sem6[dayMap[i+1]] = []
+        sem8[dayMap[i+1]] = []
+        for  slot in chromosome[i]:
+            sem2[dayMap[i+1]].append(slot[0])
+            sem4[dayMap[i+1]].append(slot[1])
+            sem6[dayMap[i+1]].append(slot[2])
+            sem8[dayMap[i+1]].append(slot[3])
+    return sem2,sem4,sem6,sem8
+                 
+    
+
+et = time.time()
+print("time : ",et-st)
+print("Max fitness achived : ",max(Fit_values))
+
+y1 , y2 , y3 , y4 = separateChromosome(pop[0])
+print("\n\n\n\nFirst year\n")
+for k , v in y1.items():
+    print(k,v)
+print("\nSecond year\n")
+for k , v in y2.items():
+    print(k,v)
+print("\nthird year\n")
+for k , v in y3.items():
+    print(k,v)
+print("\nfourth year\n")
+for k , v in y4.items():
+    print(k,v)
+print(fitnessFunction(pop[0]))
